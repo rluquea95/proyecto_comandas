352c9ef4ede1b2062246fc4769e4cc78f84c0717
Rosa <rluquea95@gmail.com>
2025-05-04 22:23:59 +0200

    Vuelta a una versión anterior

--- app/src/main/java/com/example/readytapas/data/model/ProductoPedido.kt
+++ app/src/main/java/com/example/readytapas/data/model/ProductoPedido.kt
@@ -3,5 +3,6 @@ package com.example.readytapas.data.model
 data class ProductoPedido(
     val producto: Producto = Producto(),
     var cantidad: Int = 1,
-    var preparado: Boolean = false
+    var preparado: Boolean = false,
+    var entregado: Boolean = false
 )
